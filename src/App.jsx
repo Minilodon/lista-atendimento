dd6ca9fd3918b1bd125ef4be7c937839500d4cb8
Minilodon <pedro.soareszl99@gmail.com>
2023-11-06 21:11:16 -0300

    finished configuring tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <span className='text-red-300'>Teste</span>
+    </>
+  )
+}
+
+export default App
