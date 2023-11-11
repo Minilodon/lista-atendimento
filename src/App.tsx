8731f4221095e437a212d1188d204e6f80c51e24
Minilodon <pedro.soareszl99@gmail.com>
2023-11-11 10:50:14 -0300

    Adição do Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
 
   return (
-    <div className="text-red-100">
-      Hello world!
+    <div className="w-sceen h-screen bg-red-300 flex flex-col items-center justify-center">
+      
     </div>
   )
 }
