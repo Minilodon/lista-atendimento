aff5b98392249c0bd94ed3c19019645ec1eb17a7
Minilodon <pedro.soareszl99@gmail.com>
2023-11-18 18:06:27 -0300

    Colocação das funções no contexto de workers

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
 import Button from '@mui/material/Button';
-import { useCallback, useState } from 'react';
-import { faker } from '@faker-js/faker';
 import Fab from '@mui/material/Fab';
 import ClearIcon from '@mui/icons-material/Clear';
 import { useModalContext } from './contexts/Modal/ModalContext';
-import { Worker } from './types/Worker';
 import { useWorkersContext } from './contexts/Workers/WorkersContext';
 
 
 function App() {
-  const [workers, setWorkers] = useState<Worker[]>([])
+  
   const {openDeleteModal} = useModalContext()
-  const {setSelectedWorker} = useWorkersContext()
+  const {setSelectedWorker, workers, addWorker, putFirstToLastPosition} = useWorkersContext()
 
-  const addWorker = useCallback(() => {
-    const newWorker: Worker = {id: faker.string.uuid(), name: faker.person.firstName()}
-    setWorkers([...workers, newWorker])
-  },[workers])
-
-  const putFirstToLastPosition = () => {
-    if(workers.length === 0) return
-    const workersArray = [...workers]
-    const firstWorker = workersArray.shift()
-    workersArray.push(firstWorker!)
-    setWorkers(workersArray)
-  }
-
-  const deleteWorker = (workerId: string) => {
-    const workersArray = [...workers]
-    const filteredWorkers = workersArray.filter(({id}) => workerId !== id)
-    setWorkers(filteredWorkers)
-  }
 
   return (
     <div className="w-sceen h-screen bg-red-300 flex flex-col items-center justify-center">
--- src/contexts/Workers/WorkersContextProvider.tsx
+++ src/contexts/Workers/WorkersContextProvider.tsx
@@ -1,9 +1,15 @@
-import React, { ReactNode, createContext, useState } from 'react';
+import React, { ReactNode, createContext, useCallback, useState } from 'react';
 import { Worker } from '../../types/Worker';
+import { faker } from '@faker-js/faker';
 
 interface WorkersContextValue {
   selectedWorker: Worker | undefined;
   setSelectedWorker: React.Dispatch<React.SetStateAction<Worker | undefined>>
+  workers: Worker[]
+  setWorkers: React.Dispatch<React.SetStateAction<Worker[]>>
+  addWorker: () => void
+  putFirstToLastPosition: () => void
+  deleteWorker: (workerId: string) => void
 }
 
 export const WorkersContext = createContext<WorkersContextValue>(
@@ -16,12 +22,38 @@ type WorkersContextProviderProps = {
 
 export default function WorkersContextProvider(props: WorkersContextProviderProps) {
   const { children } = props;
+
+  const [workers, setWorkers] = useState<Worker[]>([])
   const [selectedWorker, setSelectedWorker] = useState<Worker>()
+
+  const addWorker = useCallback(() => {
+    const newWorker: Worker = {id: faker.string.uuid(), name: faker.person.firstName()}
+    setWorkers([...workers, newWorker])
+  },[workers])
+
+  const putFirstToLastPosition = useCallback(() => {
+    if(workers.length === 0) return
+    const workersArray = [...workers]
+    const firstWorker = workersArray.shift()
+    workersArray.push(firstWorker!)
+    setWorkers(workersArray)
+  },[workers])
+
+  const deleteWorker = useCallback((workerId: string) => {
+    const workersArray = [...workers]
+    const filteredWorkers = workersArray.filter(({id}) => workerId !== id)
+    setWorkers(filteredWorkers)
+  },[workers])
     
   const value: WorkersContextValue = React.useMemo(() => ({
     selectedWorker,
-    setSelectedWorker
-  }), [selectedWorker]);
+    setSelectedWorker,
+    workers,
+    setWorkers, 
+    addWorker,
+    putFirstToLastPosition,
+    deleteWorker
+  }), [addWorker, deleteWorker, putFirstToLastPosition, selectedWorker, workers]);
 
   return (
     <WorkersContext.Provider value={value}>
