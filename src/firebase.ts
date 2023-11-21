34652ec703fe7edf074a9f917dad2543190e5611
Minilodon <pedro.soareszl99@gmail.com>
2023-11-21 16:53:42 -0300

    Adição do firebase

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "lista-atendimento.firebaseapp.com",
+  projectId: "lista-atendimento",
+  storageBucket: "lista-atendimento.appspot.com",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app)
