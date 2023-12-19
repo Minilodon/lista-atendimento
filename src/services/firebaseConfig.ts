847a0297e291ca06802a1c5192daf5709900667e
Minilodon <pedro.soareszl99@gmail.com>
2023-12-19 16:51:52 -0300

    Colocadas as chaves no local.env

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -4,9 +4,9 @@ import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "lista-atendimento.firebaseapp.com",
-  projectId: "lista-atendimento",
-  storageBucket: "lista-atendimento.appspot.com",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
