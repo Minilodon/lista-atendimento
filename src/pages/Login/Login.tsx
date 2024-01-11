5996b209ec1347f40e4641e230eba46de49a8864
Minilodon <pedro.soareszl99@gmail.com>
2024-01-11 14:09:03 -0300

    Ajustes no carregamento do formulário de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,6 +3,8 @@ import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "../../services/firebaseConfig";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function Login() {
 	const [email, setEmail] = useState("");
@@ -25,11 +27,13 @@ function Login() {
 				className="bg-white px-4 py-4 flex flex-col gap-y-3 shadow-md rounded-sm min-h-[340px] min-w-[320px]"
 				onSubmit={handleSignIn}
 			>
-				{loading ? (
-					<span>Carregando...</span>
-				) : (
-					<div className="flex flex-col gap-y-2 items-center">
-						<span className="self-center mb-2 text-lg">Fazer Login</span>
+				<div className="flex flex-col gap-y-2 items-center">
+					<span className="self-center mb-2 text-lg">Fazer Login</span>
+					{loading ? (
+						<div className="w-full">
+							<Skeleton width={"100%"} height={56} />
+						</div>
+					) : (
 						<TextField
 							label="E-mail"
 							variant="outlined"
@@ -38,7 +42,14 @@ function Login() {
 							value={email}
 							onChange={(e) => setEmail(e.target.value)}
 							style={{ marginBottom: "8px", width: "100%" }}
+							disabled={loading}
 						/>
+					)}
+					{loading ? (
+						<div className="w-full">
+							<Skeleton width={"100%"} height={56} />
+						</div>
+					) : (
 						<TextField
 							label="Senha"
 							variant="outlined"
@@ -46,30 +57,41 @@ function Login() {
 							value={password}
 							onChange={(e) => setPassword(e.target.value)}
 							style={{ marginBottom: "8px", width: "100%" }}
+							disabled={loading}
 						/>
-						<Link
-							to={"/recover-password"}
-							className="text-center hover:text-blue-500 transition text-sm"
-						>
-							Esqueceu a sua senha?
-						</Link>
-						<button
-							type="submit"
-							className="bg-blue-700 hover:bg-blue-500 text-white rounded-sm px-4 py-4 text-lg w-full"
-						>
-							Login
-						</button>
-						<div className="flex gap-x-2">
-							<span className="text-sm">Não possui uma conta?</span>
+					)}
+					<div className="h-6 w-full flex items-center justify-center">
+						{loading ? null : (
 							<Link
-								to={"/register"}
-								className="text-primaryBlue underline hover:text-blue-500 transition text-sm"
+								to={"/recover-password"}
+								className="text-center hover:text-blue-500 transition text-sm"
 							>
-								Crie sua conta aqui
+								Esqueceu a sua senha?
 							</Link>
-						</div>
+						)}
 					</div>
-				)}
+
+					<button
+						type="submit"
+						className="bg-blue-700 hover:bg-blue-500 text-white rounded-sm px-4 py-4 text-lg w-full transition"
+						disabled={loading}
+					>
+						Login
+					</button>
+					<div className="flex gap-x-2 h-6 w-full items-center justify-center">
+						{loading ? null : (
+							<>
+								<span className="text-sm">Não possui uma conta?</span>
+								<Link
+									to={"/register"}
+									className="text-primaryBlue underline hover:text-blue-500 transition text-sm"
+								>
+									Crie sua conta aqui
+								</Link>
+							</>
+						)}
+					</div>
+				</div>
 			</form>
 		</div>
 	);
